cbb0ce7879ff3338313cf891ff296a306fb41f1e
Ahmad Nizar <ahmadnizar@10-60-48-107.0200.bsd.id.iosda.org>
2019-04-25 09:34:25 +0700

    -init project

---
+++ physicsMiniGames/physicsMiniGames/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  physicsMiniGames
+//
+//  Created by Ahmad Nizar on 25/04/19.
+//  Copyright © 2019 Ahmad Nizar. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ physicsMiniGames/physicsMiniGames/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  physicsMiniGames
+//
+//  Created by Ahmad Nizar on 25/04/19.
+//  Copyright © 2019 Ahmad Nizar. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
