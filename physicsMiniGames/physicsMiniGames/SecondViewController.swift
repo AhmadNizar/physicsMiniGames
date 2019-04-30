3309a8350f08605a824856734b32e2a745e96d7f
Kevin <ckevin2797@gmail.com>
2019-04-30 09:22:29 +0700

    Update scoreboard

--- physicsMiniGames/physicsMiniGames/SecondViewController.swift
+++ physicsMiniGames/physicsMiniGames/SecondViewController.swift
@@ -7,12 +7,30 @@
 //
 
 import UIKit
+import Alamofire
+import SwiftyJSON
 
 class SecondViewController: UIViewController {
 
+    @IBOutlet weak var scoreTitle: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        Alamofire.request(
+            "http://10.60.48.159:3000/games/"
+            ).responseData { responseData in
+                if((responseData.result.value) != nil) {
+                    let swiftyJsonVar = JSON(responseData.result.value!)
+                    print(swiftyJsonVar)
+                    
+                    self.scoreTitle.text = swiftyJsonVar[0]["title"].string
+                }
+                
+//                self.scoreTitle.text = responseData[0]["title"].string
+                
+        }
     }
 
 
